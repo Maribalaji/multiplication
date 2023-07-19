b3e484f9796444a25fce48d1ff2028ec707b2e30
Maribalaji <maribalaji008@gmail.com>
2023-07-19 20:34:15 +0530

    add

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var a=prompt("Enter a multiplication number");
 var b=1;
 var c;
-var d=prompt("Enter limit number");
+var d=prompt("Enter limit of number");
 while(b<=d){
 	c=b*a;
 document.write(b+"*"+a+"="+c+"<br>");
